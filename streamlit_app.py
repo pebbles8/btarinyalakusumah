c28a0b2d5f516d2144fe328dd7b36ede480b0fb3
pebbles8 <nyalamaya@gmail.com>
2025-04-24 14:54:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Pebbles")
 st.write(
-    "An escape to have fun! Enjoy fellow Pebs!"
+    "An escape to have fun! Credits to my kids, Pebbles, Ellie and Batman. I love all of you. Sincerely, Your mom."
 )
