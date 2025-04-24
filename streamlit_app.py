8ec71861d78711cb48d1ba5a36e1bea8b7475b15
pebbles8 <nyalamaya@gmail.com>
2025-04-24 14:50:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Pebbles")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "An escape to have fun! Enjoy fellow Pebs!"
 )
