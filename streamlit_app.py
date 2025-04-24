2b39cac01da32e73b77d7a8d29a3b12f53f7491e
pebbles8 <nyalamaya@gmail.com>
2025-04-24 15:13:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("Pebbles")
 st.write(
-    "An escape to have fun! Credits to my kids, Pebbles, Ellie and Batman. I love all of you. Sincerely, Your mom."
+    "Btari Nyala Kusumah, an avid movie watcher"
 )
